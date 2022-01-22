13cd95e9ebd53fb664f4f3b0faa2be18d4b2e8a1
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-01-22 15:11:11 -0500

    Update htmlExport.js

--- htmlExport.js
+++ htmlExport.js
@@ -1,5 +1,5 @@
 function exportHTML() {
 	var code = Blockly.JavaScript.workspaceToCode(workspace);
-	var blob = new Blob(["<title>" + document.getElementById('gamename').value + "</title>\n<script>" + code + "</script>"], {type: "text/plain;charset=utf-8"});
+	var blob = new Blob(["<html>\n    <head>\n        <title>" + document.getElementById('gamename').value + "</title>\n    <head>\n    <body>\n    </body>\n</html>\n<script>\n" + code + "</script>"], {type: "text/plain;charset=utf-8"});
 	saveAs(blob, document.getElementById('gamename').value + ".html");
 }
